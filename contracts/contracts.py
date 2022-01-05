91e13f8bb2065971a63c99e73e4eb719e7026a5a
tallywiesenberg <t.j.wies@gmail.com>
2022-01-05 15:00:55 +0100

    highest level application

---
+++ contracts/contracts.py
@@ -0,0 +1,23 @@
+from pyteal import *
+
+def approval_program():
+
+
+    program = Cond(
+        [Txn.application_id() == Int(0), on_create],
+        [Txn.on_completion() == OnComplete.NoOp, on_call],
+        [
+            Txn.on_completion() == OnComplete.OptIn,
+            on_delete
+        ],
+        [
+            Or(
+                Txn.on_completion() == OnComplete.OptIn,
+                Txn.on_completion() == OnComplete.CloseOut,
+                Txn.on_completion() == OnComplete.UpdateApplication,
+            ),
+            Reject()
+        ]
+    )
+
+    return program
