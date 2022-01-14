5dfce00c0ff237854fabbc3289af348c4422beb1
tallywiesenberg <t.j.wies@gmail.com>
2022-01-14 14:52:06 -0500

    add utils and refactor functions

---
+++ tellorflex/deploy.py
@@ -0,0 +1,90 @@
+from typing import Tuple, List
+
+from algosdk.v2client.algod import AlgodClient
+from algosdk.future import transaction
+from algosdk.logic import get_application_address
+from algosdk import account, encoding
+
+from pyteal import compileTeal, Mode
+
+from .account import Account
+from .contracts import approval_program, clear_state_program
+from .util import (
+    waitForTransaction,
+    fullyCompileContract,
+    getAppGlobalState,
+)
+
+APPROVAL_PROGRAM = b""
+CLEAR_STATE_PROGRAM = b""
+
+
+def getContracts(client: AlgodClient) -> Tuple[bytes, bytes]:
+    """Get the compiled TEAL contracts for the auction.
+    Args:
+        client: An algod client that has the ability to compile TEAL programs.
+    Returns:
+        A tuple of 2 byte strings. The first is the approval program, and the
+        second is the clear state program.
+    """
+    global APPROVAL_PROGRAM
+    global CLEAR_STATE_PROGRAM
+
+    if len(APPROVAL_PROGRAM) == 0:
+        APPROVAL_PROGRAM = fullyCompileContract(client, approval_program())
+        CLEAR_STATE_PROGRAM = fullyCompileContract(client, clear_state_program())
+
+    return APPROVAL_PROGRAM, CLEAR_STATE_PROGRAM
+
+def deploy_tellor_flex(
+    client: AlgodClient,
+    sender: Account,
+    governance_address: Account,
+    stake_amount: int,
+    query_id: str,
+    query_data: str
+) -> int:
+    """Create a new auction.
+    Args:
+        client: An algod client.
+        sender: The account that will create the auction application.
+        governance_address: the account that can vote to change the
+                            properties of the contract
+        stake_amount: the amount of tokens needed to submit a value
+        query_id: the ID of the data requested to be put on chain
+        query_data: the in-depth specifications of the data requested
+    Returns:
+        The ID of the newly created auction app.
+    """
+    approval, clear = getContracts(client)
+
+    globalSchema = transaction.StateSchema(num_uints=7, num_byte_slices=2)
+    localSchema = transaction.StateSchema(num_uints=0, num_byte_slices=0)
+
+    app_args = [
+        encoding.decode_address(governance_address),
+        nftID.to_bytes(8, "big"),
+        startTime.to_bytes(8, "big"),
+        endTime.to_bytes(8, "big"),
+        reserve.to_bytes(8, "big"),
+        minBidIncrement.to_bytes(8, "big"),
+    ]
+
+    txn = transaction.ApplicationCreateTxn(
+        sender=sender.getAddress(),
+        on_complete=transaction.OnComplete.NoOpOC,
+        approval_program=approval,
+        clear_program=clear,
+        global_schema=globalSchema,
+        local_schema=localSchema,
+        app_args=app_args,
+        sp=client.suggested_params(),
+    )
+
+    signedTxn = txn.sign(sender.getPrivateKey())
+
+    client.send_transaction(signedTxn)
+
+    response = waitForTransaction(client, signedTxn.get_txid())
+    assert response.applicationIndex is not None and response.applicationIndex > 0
+    return response.applicationIndex
