ef072f565f70f64e7e4790cb4bd6eec6bd61f71d
tallywiesenberg <t.j.wies@gmail.com>
2022-01-19 13:06:08 -0500

    added template files

--- tellorflex/contracts.py
+++ tellorflex/contracts.py
@@ -26,3 +26,16 @@ def approval_program():
     )
 
     return program
+
+def clear_state_program():
+    return Approve()
+
+
+if __name__ == "__main__":
+    with open("auction_approval.teal", "w") as f:
+        compiled = compileTeal(approval_program(), mode=Mode.Application, version=5)
+        f.write(compiled)
+
+    with open("auction_clear_state.teal", "w") as f:
+        compiled = compileTeal(clear_state_program(), mode=Mode.Application, version=5)
+        f.write(compiled)
--- tellorflex/deploy.py
+++ tellorflex/deploy.py
@@ -19,8 +19,8 @@ APPROVAL_PROGRAM = b""
 CLEAR_STATE_PROGRAM = b""
 
 
-def getContracts(client: AlgodClient) -> Tuple[bytes, bytes]:
-    """Get the compiled TEAL contracts for the auction.
+def get_contracts(client: AlgodClient) -> Tuple[bytes, bytes]:
+    """Get the compiled TEAL contracts for the tellor contract.
     Args:
         client: An algod client that has the ability to compile TEAL programs.
     Returns:
@@ -44,7 +44,7 @@ def deploy_tellor_flex(
     query_id: str,
     query_data: str
 ) -> int:
-    """Create a new auction.
+    """Create a new tellor reporting contract.
     Args:
         client: An algod client.
         sender: The account that will create the auction application.
@@ -56,18 +56,16 @@ def deploy_tellor_flex(
     Returns:
         The ID of the newly created auction app.
     """
-    approval, clear = getContracts(client)
+    approval, clear = get_contracts(client)
 
     globalSchema = transaction.StateSchema(num_uints=7, num_byte_slices=2)
     localSchema = transaction.StateSchema(num_uints=0, num_byte_slices=0)
 
     app_args = [
         encoding.decode_address(governance_address),
-        nftID.to_bytes(8, "big"),
-        startTime.to_bytes(8, "big"),
-        endTime.to_bytes(8, "big"),
-        reserve.to_bytes(8, "big"),
-        minBidIncrement.to_bytes(8, "big"),
+        stake_amount.to_bytes(8, "big"),
+        query_id.to_bytes(32, "big"),
+        query_data.to_bytes(256, "big"),
     ]
 
     txn = transaction.ApplicationCreateTxn(
@@ -88,3 +86,162 @@ def deploy_tellor_flex(
     response = waitForTransaction(client, signedTxn.get_txid())
     assert response.applicationIndex is not None and response.applicationIndex > 0
     return response.applicationIndex
+
+def setup_tellor_flex(
+    client: AlgodClient,
+    appID: int,
+    funder: Account,
+    nftHolder: Account,
+    nftID: int,
+    nftAmount: int,
+) -> None:
+    """Finish setting up an auction.
+    This operation funds the app auction escrow account, opts that account into
+    the NFT, and sends the NFT to the escrow account, all in one atomic
+    transaction group. The auction must not have started yet.
+    The escrow account requires a total of 0.203 Algos for funding. See the code
+    below for a breakdown of this amount.
+    Args:
+        client: An algod client.
+        appID: The app ID of the auction.
+        funder: The account providing the funding for the escrow account.
+        nftHolder: The account holding the NFT.
+        nftID: The NFT ID.
+        nftAmount: The NFT amount being auctioned. Some NFTs has a total supply
+            of 1, while others are fractional NFTs with a greater total supply,
+            so use a value that makes sense for the NFT being auctioned.
+    """
+    appAddr = get_application_address(appID)
+
+    suggestedParams = client.suggested_params()
+
+    fundingAmount = (
+        # min account balance
+        100_000
+        # additional min balance to opt into NFT
+        + 100_000
+        # 3 * min txn fee
+        + 3 * 1_000
+    )
+
+    fundAppTxn = transaction.PaymentTxn(
+        sender=funder.getAddress(),
+        receiver=appAddr,
+        amt=fundingAmount,
+        sp=suggestedParams,
+    )
+
+    setupTxn = transaction.ApplicationCallTxn(
+        sender=funder.getAddress(),
+        index=appID,
+        on_complete=transaction.OnComplete.NoOpOC,
+        app_args=[b"setup"],
+        foreign_assets=[nftID],
+        sp=suggestedParams,
+    )
+
+    fundNftTxn = transaction.AssetTransferTxn(
+        sender=nftHolder.getAddress(),
+        receiver=appAddr,
+        index=nftID,
+        amt=nftAmount,
+        sp=suggestedParams,
+    )
+
+    transaction.assign_group_id([fundAppTxn, setupTxn, fundNftTxn])
+
+    signedFundAppTxn = fundAppTxn.sign(funder.getPrivateKey())
+    signedSetupTxn = setupTxn.sign(funder.getPrivateKey())
+    signedFundNftTxn = fundNftTxn.sign(nftHolder.getPrivateKey())
+
+    client.send_transactions([signedFundAppTxn, signedSetupTxn, signedFundNftTxn])
+
+    waitForTransaction(client, signedFundAppTxn.get_txid())
+
+
+def placeBid(client: AlgodClient, appID: int, bidder: Account, bidAmount: int) -> None:
+    """Place a bid on an active auction.
+    Args:
+        client: An Algod client.
+        appID: The app ID of the auction.
+        bidder: The account providing the bid.
+        bidAmount: The amount of the bid.
+    """
+    appAddr = get_application_address(appID)
+    appGlobalState = getAppGlobalState(client, appID)
+
+    nftID = appGlobalState[b"nft_id"]
+
+    if any(appGlobalState[b"bid_account"]):
+        # if "bid_account" is not the zero address
+        prevBidLeader = encoding.encode_address(appGlobalState[b"bid_account"])
+    else:
+        prevBidLeader = None
+
+    suggestedParams = client.suggested_params()
+
+    payTxn = transaction.PaymentTxn(
+        sender=bidder.getAddress(),
+        receiver=appAddr,
+        amt=bidAmount,
+        sp=suggestedParams,
+    )
+
+    appCallTxn = transaction.ApplicationCallTxn(
+        sender=bidder.getAddress(),
+        index=appID,
+        on_complete=transaction.OnComplete.NoOpOC,
+        app_args=[b"bid"],
+        foreign_assets=[nftID],
+        # must include the previous lead bidder here to the app can refund that bidder's payment
+        accounts=[prevBidLeader] if prevBidLeader is not None else [],
+        sp=suggestedParams,
+    )
+
+    transaction.assign_group_id([payTxn, appCallTxn])
+
+    signedPayTxn = payTxn.sign(bidder.getPrivateKey())
+    signedAppCallTxn = appCallTxn.sign(bidder.getPrivateKey())
+
+    client.send_transactions([signedPayTxn, signedAppCallTxn])
+
+    waitForTransaction(client, appCallTxn.get_txid())
+
+
+def closeAuction(client: AlgodClient, appID: int, closer: Account):
+    """Close an auction.
+    This action can only happen before an auction has begun, in which case it is
+    cancelled, or after an auction has ended.
+    If called after the auction has ended and the auction was successful, the
+    NFT is transferred to the winning bidder and the auction proceeds are
+    transferred to the seller. If the auction was not successful, the NFT and
+    all funds are transferred to the seller.
+    Args:
+        client: An Algod client.
+        appID: The app ID of the auction.
+        closer: The account initiating the close transaction. This must be
+            either the seller or auction creator if you wish to close the
+            auction before it starts. Otherwise, this can be any account.
+    """
+    appGlobalState = getAppGlobalState(client, appID)
+
+    nftID = appGlobalState[b"nft_id"]
+
+    accounts: List[str] = [encoding.encode_address(appGlobalState[b"seller"])]
+
+    if any(appGlobalState[b"bid_account"]):
+        # if "bid_account" is not the zero address
+        accounts.append(encoding.encode_address(appGlobalState[b"bid_account"]))
+
+    deleteTxn = transaction.ApplicationDeleteTxn(
+        sender=closer.getAddress(),
+        index=appID,
+        accounts=accounts,
+        foreign_assets=[nftID],
+        sp=client.suggested_params(),
+    )
+    signedDeleteTxn = deleteTxn.sign(closer.getPrivateKey())
+
+    client.send_transaction(signedDeleteTxn)
+
+    waitForTransaction(client, signedDeleteTxn.get_txid())
