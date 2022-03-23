56d779c2f54114a33b6135b2f2f24aaa497e5a24
akrem <akremstudy@gmail.com>
2022-03-23 07:15:36 -0400

    median contract fixed plus tox style

--- src/contracts/medianizer_contract.py
+++ src/contracts/medianizer_contract.py
@@ -1,104 +1,229 @@
 from pyteal import *
-'''
+
+"""
 Medianizer for feed i.e. BTC/USD
 
-'''
+"""
 
 time_interval = Bytes("time_interval")
-median_timestamp = Int(0)
+median_timestamp = Bytes("median_timestamp")
 median_price = Bytes("median")
+app_1 = Bytes("app_1")
+app_2 = Bytes("app_2")
+app_3 = Bytes("app_3")
+app_4 = Bytes("app_4")
+app_5 = Bytes("app_5")
 
-# TODO: ensure this is the name of variable in feed contract to read from
-price =Bytes("price")
+# this is the name of variable in feed contract to read from
+last_value = Bytes("last_value")
 
-# TODO: Feed contract ids, application ids might not be necessary.
+governance = Bytes("governance")
+is_governance = Txn.sender() == App.globalGet(governance)
+is_valid_feed = Btoi(Txn.sender()) == Or(
+    Btoi(App.globalGet(app_1)),
+    Btoi(App.globalGet(app_2)),
+    Btoi(App.globalGet(app_3)),
+    Btoi(App.globalGet(app_4)),
+    Btoi(App.globalGet(app_5)),
+)
 
-teamsig = Bytes("teamsig")
-is_team = Txn.sender() == teamsig
 
 def create():
-    return Seq([
-        App.globalPut(teamsig, Txn.application_args[1]),
+    """
+    constructor for medianizer contract
+
+    args:
+    0) governance address
+    1) time interval that checks age of values
+
+    """
+    return Seq(
+        App.globalPut(governance, Txn.application_args[1]),
         App.globalPut(time_interval, Txn.application_args[2]),
-        Approve()
-    ])
+        Approve(),
+    )
+
+
+def activate_contract():
+    """
+    Only governance sets the application(aka feed) ids
+    that are able to use this medianizer
+
+    Txn args:
+    0) will always equal "activate_contract"
+
+    """
+    addr_1 = AppParam.address(Int(1))
+    addr_2 = AppParam.address(Int(2))
+    addr_3 = AppParam.address(Int(3))
+    addr_4 = AppParam.address(Int(4))
+    addr_5 = AppParam.address(Int(5))
+    return Seq(
+        Assert(is_governance),
+        addr_1,
+        addr_2,
+        addr_3,
+        addr_4,
+        addr_5,
+        App.globalPut(app_1, addr_1.value()),
+        App.globalPut(app_2, addr_2.value()),
+        App.globalPut(app_3, addr_3.value()),
+        App.globalPut(app_4, addr_4.value()),
+        App.globalPut(app_5, addr_5.value()),
+        Approve(),
+    )
+
+
+def get_values():
+    """
+    Gets values and timestamp of last report from all feeds
+    and checks timestamp age against time_interval then passes
+    values to medinizer function to get the median and stores globally
+
+    Txn args:
+    0) will always equal "get_values"
+
+    """
+
+    feed_1_value = App.globalGetEx(Int(1), last_value)
+    feed_2_value = App.globalGetEx(Int(2), last_value)
+    feed_3_value = App.globalGetEx(Int(3), last_value)
+    feed_4_value = App.globalGetEx(Int(4), last_value)
+    feed_5_value = App.globalGetEx(Int(5), last_value)
+
+    var1 = ScratchVar(TealType.uint64)
+    var2 = ScratchVar(TealType.uint64)
+    var3 = ScratchVar(TealType.uint64)
+    var4 = ScratchVar(TealType.uint64)
+    var5 = ScratchVar(TealType.uint64)
+
+    validate_prices = Seq(
+        If(
+            And(
+                feed_1_value.hasValue(),
+                Minus(Btoi(Substring(feed_1_value.value(), Int(11), Int(20))), Global.latest_timestamp())
+                > App.globalGet(time_interval),
+            )
+        )
+        .Then(var1.store(Btoi(Substring(feed_1_value.value(), Int(1), Int(10)))))
+        .Else(var1.store(Int(0))),
+        If(
+            And(
+                feed_2_value.hasValue(),
+                Minus(Btoi(Substring(feed_2_value.value(), Int(11), Int(20))), Global.latest_timestamp())
+                > App.globalGet(time_interval),
+            )
+        )
+        .Then(var2.store(Btoi(Substring(feed_2_value.value(), Int(1), Int(10)))))
+        .Else(var2.store(Int(0))),
+        If(
+            And(
+                feed_3_value.hasValue(),
+                Minus(Btoi(Substring(feed_3_value.value(), Int(11), Int(20))), Global.latest_timestamp())
+                > App.globalGet(time_interval),
+            )
+        )
+        .Then(var3.store(Btoi(Substring(feed_3_value.value(), Int(1), Int(10)))))
+        .Else(var3.store(Int(0))),
+        If(
+            And(
+                feed_4_value.hasValue(),
+                Minus(Btoi(Substring(feed_4_value.value(), Int(11), Int(20))), Global.latest_timestamp())
+                > App.globalGet(time_interval),
+            )
+        )
+        .Then(var4.store(Btoi(Substring(feed_4_value.value(), Int(1), Int(10)))))
+        .Else(var4.store(Int(0))),
+        If(
+            And(
+                feed_5_value.hasValue(),
+                Minus(Btoi(Substring(feed_5_value.value(), Int(11), Int(20))), Global.latest_timestamp())
+                > App.globalGet(time_interval),
+            )
+        )
+        .Then(var5.store(Btoi(Substring(feed_5_value.value(), Int(1), Int(10)))))
+        .Else(var5.store(Int(0))),
+    )
+
+    return Seq(
+        Assert(is_valid_feed),
+        feed_1_value,
+        feed_2_value,
+        feed_3_value,
+        feed_4_value,
+        feed_5_value,
+        validate_prices,
+        App.globalPut(median_price, Medianizer(var1.load(), var2.load(), var3.load(), var4.load(), var5.load())),
+        If(App.globalGet(median_price) == var1.load())
+        .Then(App.globalPut(median_timestamp, Btoi(Substring(feed_1_value.value(), Int(11), Int(20)))))
+        .ElseIf(App.globalGet(median_price) == var2.load())
+        .Then(App.globalPut(median_timestamp, Btoi(Substring(feed_2_value.value(), Int(11), Int(20)))))
+        .ElseIf(App.globalGet(median_price) == var3.load())
+        .Then(App.globalPut(median_timestamp, Btoi(Substring(feed_3_value.value(), Int(11), Int(20)))))
+        .ElseIf(App.globalGet(median_price) == var4.load())
+        .Then(App.globalPut(median_timestamp, Btoi(Substring(feed_4_value.value(), Int(11), Int(20)))))
+        .ElseIf(App.globalGet(median_price) == var5.load())
+        .Then(App.globalPut(median_timestamp, Btoi(Substring(feed_5_value.value(), Int(11), Int(20))))),
+        Approve(),
+    )
+
 
 @Subroutine(TealType.uint64)
-def Medianizer(
-    a: Expr, 
-    b: Expr, 
-    c: Expr,
-    d: Expr,
-    e: Expr):
+def Medianizer(a: Expr, b: Expr, c: Expr, d: Expr, e: Expr) -> TealType.uint64:
+    """
+    function for getting the middle value
+
+    sorts values one at a time
+    until all values are sorted
+
+    if number of values is odd, middle num is picked
+    else divides middle two values and divides by 2
+    """
+
     tmp = ScratchVar(TealType.uint64)
-    
-    sort_1 = Seq([
-        
+
+    sort_1 = Seq(
         tmp.store(a),
         If(And(b > a, b > c, b > d, b > e))
-        .Then(
-            Seq(a.slot.store(b), b.slot.store(tmp.load()))
-            )
+        .Then(Seq(a.slot.store(b), b.slot.store(tmp.load())))
         .ElseIf(And(c > b, c > a, c > d, c > e))
-        .Then(
-            Seq(a.slot.store(c), c.slot.store(tmp.load()))
-            )
+        .Then(Seq(a.slot.store(c), c.slot.store(tmp.load())))
         .ElseIf(And(d > b, d > a, d > c, d > e))
-        .Then(
-            Seq(a.slot.store(d), d.slot.store(tmp.load()))
-            )
+        .Then(Seq(a.slot.store(d), d.slot.store(tmp.load())))
         .ElseIf(And(e > b, e > a, e > c, e > d))
-        .Then(
-            Seq(a.slot.store(e), e.slot.store(tmp.load()))
-            )
-        ])
+        .Then(Seq(a.slot.store(e), e.slot.store(tmp.load()))),
+    )
 
-    sort_2 = Seq([
+    sort_2 = Seq(
         tmp.store(b),
         If(And(c > b, c > d, c > e))
-        .Then(
-            Seq(b.slot.store(c), c.slot.store(tmp.load()))
-            )
+        .Then(Seq(b.slot.store(c), c.slot.store(tmp.load())))
         .ElseIf(And(d > b, d > c, d > e))
-        .Then(
-            Seq(b.slot.store(d), d.slot.store(tmp.load()))
-            )
+        .Then(Seq(b.slot.store(d), d.slot.store(tmp.load())))
         .ElseIf(And(e > b, e > c, e > d))
-        .Then(
-            Seq(b.slot.store(e), e.slot.store(tmp.load()))
-            )
-    ])
-    sort_3 = Seq([
+        .Then(Seq(b.slot.store(e), e.slot.store(tmp.load()))),
+    )
+    sort_3 = Seq(
         tmp.store(c),
         If(And(d > c, d > e))
-        .Then(
-            Seq(c.slot.store(d), d.slot.store(tmp.load()))
-            )
+        .Then(Seq(c.slot.store(d), d.slot.store(tmp.load())))
         .ElseIf(And(e > c, e > d))
-        .Then(
-            Seq(c.slot.store(e), e.slot.store(tmp.load()))
-            )
-    ])
+        .Then(Seq(c.slot.store(e), e.slot.store(tmp.load()))),
+    )
 
-    sort_4 = Seq([
-        tmp.store(d),
-        If(e > d)
-        .Then(
-            Seq(d.slot.store(e), e.slot.store(tmp.load()))
-            )
-    ])
-    
+    sort_4 = Seq(tmp.store(d), If(e > d).Then(Seq(d.slot.store(e), e.slot.store(tmp.load()))))
+
     i = ScratchVar(TealType.uint64)
-    x = Seq([
+    value_count = Seq(
         i.store(Int(0)),
-        If(a > Int(0), i.store(i.load()+Int(1))),
-        If(b > Int(0), i.store(i.load()+Int(1))),
-        If(c > Int(0), i.store(i.load()+Int(1))),
-        If(d > Int(0), i.store(i.load()+Int(1))),
-        If(e > Int(0), i.store(i.load()+Int(1)))
-    ])
-    
-    y = Seq([
+        If(a > Int(0), i.store(i.load() + Int(1))),
+        If(b > Int(0), i.store(i.load() + Int(1))),
+        If(c > Int(0), i.store(i.load() + Int(1))),
+        If(d > Int(0), i.store(i.load() + Int(1))),
+        If(e > Int(0), i.store(i.load() + Int(1))),
+    )
+
+    middle_value = Seq(
         If(i.load() == Int(5))
         .Then(c.slot.load())
         .ElseIf(i.load() == Int(4))
@@ -110,56 +235,17 @@ def Medianizer(
         .ElseIf(i.load() == Int(1))
         .Then(a.slot.load())
         .Else(Int(1))
-    ])
-    
-    return Seq([
-        sort_1,
-        sort_2,
-        sort_3,
-        sort_4,
-        x,
-        y
-    ])
+    )
+    return Seq(sort_1, sort_2, sort_3, sort_4, value_count, middle_value)
 
-def get_values():
-    feed_1_value = App.globalGetEx(Int(1), price)
-    feed_2_value = App.globalGetEx(Int(2), price)
-    feed_3_value = App.globalGetEx(Int(3), price)
-    feed_4_value = App.globalGetEx(Int(4), price)
-    feed_5_value = App.globalGetEx(Int(5), price)
 
-    var1 = ScratchVar(TealType.uint64)
-    var2 = ScratchVar(TealType.uint64)
-    var3 = ScratchVar(TealType.uint64)
-    var4 = ScratchVar(TealType.uint64)
-    var5 = ScratchVar(TealType.uint64)
-
-    # TODO: for each feed_value slice the values and get last value
-    # TODO: for each feed_timestamp slice the times and get last timestamp
-    # TODO: check current timestamp and compare with feed timestamp
-    validate_prices = Seq([
-        If(And(feed_1_value.hasValue(), (median_timestamp - Global.latest_timestamp()) > time_interval))
-        .Then(var1.store(feed_1_value.value()))
-        # Substring(feed_1_value.value(), Int(0), Int)
-        .Else(var1.store(Int(0))),
-        If(And(feed_2_value.hasValue(), time_interval < median_timestamp))
-        .Then(var2.store(feed_2_value.value()))
-        .Else(var2.store(Int(0))),
-        If(And(feed_3_value.hasValue(), time_interval < median_timestamp))
-        .Then(var3.store(feed_3_value.value()))
-        .Else(var3.store(Int(0))),
-        If(And(feed_4_value.hasValue(), time_interval < median_timestamp))
-        .Then(var4.store(feed_4_value.value()))
-        .Else(var4.store(Int(0))),
-        If(And(feed_5_value.hasValue(), time_interval < median_timestamp))
-        .Then(var5.store(feed_5_value.value()))
-        .Else(var5.store(Int(0)))
-    ])
-
-    return Seq([
-        App.globalPut(median_price, Medianizer(var1,var2,var3,var4,var5)),
-        # TODO: slice timestamp or nah
-        # TODO: slice feed value for timestamp
-        If(App.globalGet(median_price) == var1.load(), App.globalPut(median_timestamp, feed_1_value)),
-        Approve()
-    ])
+def handle_method():
+    """
+    calls the appropriate contract method if
+    a NoOp transaction is sent to the contract
+    """
+    contract_method = Txn.application_args[0]
+    return Cond(
+        [contract_method == Bytes("activate_contract"), activate_contract()],
+        [contract_method == Bytes("get_values"), get_values()],
+    )
