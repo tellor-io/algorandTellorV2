e189c02409fd1a1d4016a705d3bc477b38b8cd54
tallywiesenberg <t.j.wies@gmail.com>
2022-03-29 11:41:18 -0400

    changes to config, activate contract script

--- src/scripts/activate_contract.py
+++ src/scripts/activate_contract.py
@@ -1,6 +1,12 @@
+from http import client
+import sys
 from typing import List
 from src.scripts.multisig.multisig import send_multisig_tx
+from utils.configs import get_configs
+from algosdk.algod import AlgodClient
 
-def activate_contract(app_id:int, app_ids:List[int]):
+from utils.util import getAppGlobalState
+
+def activate_contract(medianizer_id:int, feed_ids:List[int]):
     
-    send_multisig_tx(app_id=app_id, fn_name="activate_contract", app_args=None, foreign_apps=app_ids)
+    send_multisig_tx(app_id=medianizer_id, fn_name="activate_contract", app_args=None, foreign_apps=feed_ids)
