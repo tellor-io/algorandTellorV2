76a5c778e33c3dd67d8e245c750dfa227952dbbb
tallywiesenberg <t.j.wies@gmail.com>
2022-03-29 13:00:50 -0400

    slash reporter script

--- src/scripts/slash_reporter.py
+++ src/scripts/slash_reporter.py
@@ -1,7 +1,10 @@
-def slash_reporter(app_id:int):
+from scripts.multisig.multisig import send_multisig_tx
+
+
+def slash_reporter(feed_id:int):
     """
     Use the governance contract to approve or deny a value
     calls slash_reporter() on the contract, only callable by governance address
     """
 
-    pass
+    send_multisig_tx(app_id=feed_id, fn_name="slash_reporter", app_args=None, foreign_apps=None)
