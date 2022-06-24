dfb1499bb657444d163cb5ed6ac9a7090f6606ca
tallywiesenberg <t.j.wies@gmail.com>
2022-06-24 09:40:51 -0400

    creating accounts

--- src/scripts/create_accounts.py
+++ src/scripts/create_accounts.py
@@ -3,12 +3,13 @@ from algosdk import mnemonic
 from algosdk.future.transaction import Multisig
 
 
-def create_multisig():
+def create_accounts():
     """
     Prints to terminal new addresses,
     accounts, and mnemonics (private keys) for...
     - two multis signers
     - a multisig with the two signers
+    - a reporter
     """
 
     accounts = ["multis1", "multis2"]
@@ -25,7 +26,12 @@ def create_multisig():
 
     governance = Multisig(version=1, threshold=2, addresses=multisig_accounts_pk)
 
+    reporter_private_key, reporter_public_address = account.generate_account()
+
+    print("reporter mnemonic: ", mnemonic.from_private_key(reporter_private_key))
+    print("reporter address: ", reporter_public_address, "\n")
+
     print("Multisig address: ", governance.address())
 
 
-create_multisig()
+create_accounts()
